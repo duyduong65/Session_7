b071e5d899f11b392166e055ad93393866968cd6
duyduong65 <tg.bluesky66@gmail.com>
2019-09-13 13:02:56 +0700

    final funny Circle

---
+++ L_Funny_Circle/Main_Funny_Circle.js
@@ -0,0 +1,44 @@
+function Circle(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+}
+
+function getRandomHex() {
+    return Math.floor(Math.random() * 255);
+}
+
+function getRandomColor() {
+    let red = getRandomHex();
+    let green = getRandomHex();
+    let blue = getRandomHex();
+    return "rgb(" + red + "," + blue + "," + green + ")";
+}
+
+
+function CreateCircle() {
+    let radius = Math.floor(Math.random() * 100);
+    let ctx = document.getElementById("MyCanvas").getContext("2d");
+    let color = getRandomColor();
+    let x = Math.random() * (window.innerWidth - radius);
+    let y = Math.random() * (window.innerHeight - radius);
+    let circle = new Circle(x, y, radius);
+
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+
+}
+
+let n = 1;
+
+function action() {
+    for (let i = n; i > 0; i--) {
+        CreateCircle();
+        setTimeout(action, 500)
+
+    }
+
+}
+action();
