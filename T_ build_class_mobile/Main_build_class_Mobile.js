88172fa0a2f49e43492fa8b9168e667cb0d511fd
duyduong65 <tg.bluesky66@gmail.com>
2019-09-16 14:08:37 +0700

    final class mobile part 3

--- T_ build_class_mobile/Main_build_class_Mobile.js
+++ T_ build_class_mobile/Main_build_class_Mobile.js
@@ -1,5 +1,5 @@
-let Mobile = function (battery, inbox, sent, draft) {
-    this.battery = battery;
+let Mobile = function (inbox, sent, draft) {
+
     this.inbox = inbox;
     this.sent = sent;
     this.draft = draft;
@@ -18,12 +18,6 @@ let Mobile = function (battery, inbox, sent, draft) {
         return document.getElementById('messageIphone').value;
     };
 
-    this.getBatteryNokia = function () {
-        document.getElementById('batteryNokia').innerHTML = `Battery: ${this.battery} đv`;
-    };
-    this.getBatteryIphone = function () {
-        document.getElementById('batteryIphone').innerHTML = `Battery: ${this.battery} đv`;
-    };
     this.getSendMessageToIphone = function () {
         return this.getMessageNokia();
     };
@@ -32,49 +26,65 @@ let Mobile = function (battery, inbox, sent, draft) {
     };
 
 };
-let nokia = new Mobile(25, [], [], []);
-nokia.getBatteryNokia();
+let batteryNokia = 100;
+let nokia = new Mobile([], [], []);
+document.getElementById('batteryNokia').innerHTML = `Battery: ${batteryNokia} đv`;
 
 function sentToIphone() {
     nokia.getInbox().push(nokia.getSendMessageToIphone());
     nokia.getSent().push(nokia.getSendMessageToIphone());
     document.getElementById('messageNokia').value = "";
     document.getElementById('messageNokia').focus();
+    batteryNokia--;
+    document.getElementById('batteryNokia').innerHTML = `Battery: ${batteryNokia} đv`;
 }
 
 function showInboxIphone() {
+    document.getElementById('showInboxIphone').innerHTML = "";
     for (let i = 0; i < nokia.getInbox().length; i++) {
         document.getElementById('showInboxIphone').innerHTML += `Message ${i + 1} : ${nokia.getInbox()[i]} <br>`;
     }
+    batteryIphone--;
+    document.getElementById('batteryIphone').innerHTML = `Battery: ${batteryIphone} đv`;
 }
 
 function showSentIphone() {
+    document.getElementById('showSentNokia').innerHTML = "";
     for (let i = 0; i < nokia.getInbox().length; i++) {
         document.getElementById('showSentNokia').innerHTML += `Message ${i + 1} : ${nokia.getInbox()[i]} <br>`;
     }
+    batteryNokia--;
+    document.getElementById('batteryNokia').innerHTML = `Battery: ${batteryNokia} đv`;
 }
 
-let iphone = new Mobile(78, [], [], []);
-
-iphone.getBatteryIphone();
+let batteryIphone = 100;
+let iphone = new Mobile([], [], []);
+document.getElementById('batteryIphone').innerHTML = `Battery: ${batteryIphone} đv`;
 
 function sentToNokia() {
     iphone.getInbox().push(iphone.getSendMessageToNokia());
     iphone.getSent().push(iphone.getSendMessageToNokia());
     document.getElementById('messageIphone').value = "";
     document.getElementById('messageIphone').focus();
+    batteryIphone--;
+    document.getElementById('batteryIphone').innerHTML = `Battery: ${batteryIphone} đv`;
 }
 
 function showInboxNokia() {
+    document.getElementById('showInboxNokia').innerHTML = "";
     for (let i = 0; i < iphone.getInbox().length; i++) {
         document.getElementById('showInboxNokia').innerHTML += `Message ${i + 1} : ${iphone.getInbox()[i]} <br>`;
     }
+    batteryNokia--;
+    document.getElementById('batteryNokia').innerHTML = `Battery: ${batteryNokia} đv`;
 }
 
 function showSentNokia() {
+    document.getElementById('showSentIphone').innerHTML = "";
     for (let i = 0; i < iphone.getInbox().length; i++) {
         document.getElementById('showSentIphone').innerHTML += `Message ${i + 1} : ${iphone.getInbox()[i]} <br>`;
     }
-
+    batteryIphone--;
+    document.getElementById('batteryIphone').innerHTML = `Battery: ${batteryIphone} đv`;
 }
 
